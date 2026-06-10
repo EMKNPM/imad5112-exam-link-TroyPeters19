54f4df69dd499df62889bea4b30f0845677db73d
TroyPeters19 <St10533034@myemeris.edu.za>
2026-06-10 12:24:43 +0200

    Padding adjusted
    Padding adjusted to avoid the content from overlapping the navigation options

--- IMAD_Exam/app/src/main/java/com/example/imad_exam/DetailedActivity.kt
+++ IMAD_Exam/app/src/main/java/com/example/imad_exam/DetailedActivity.kt
@@ -88,6 +88,21 @@ class DetailedActivity : AppCompatActivity() {
             }
         }
 
+        // Padding is adjusted so the content does not overlap the navigation options.
+        // Improves the layout of the app
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+
+            v.setPadding(
+                systemBars.left,
+                systemBars.top,
+                systemBars.right,
+                systemBars.bottom
+            )
+
+            insets
+        }
+
 
 
 
