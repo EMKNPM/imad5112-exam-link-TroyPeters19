8c1b4437f8b31f25be3745104e3a97d3651c00fc
TroyPeters19 <St10533034@myemeris.edu.za>
2026-06-10 09:47:22 +0200

    Declarations and Typecasting
    Variables declared and typecasting is done

--- IMAD_Exam/app/src/main/java/com/example/imad_exam/MainScreenActivity.kt
+++ IMAD_Exam/app/src/main/java/com/example/imad_exam/MainScreenActivity.kt
@@ -1,16 +1,34 @@
 package com.example.imad_exam
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainScreenActivity : AppCompatActivity() {
+
+    // Declarations
+    private lateinit var tvTotal: TextView
+    private lateinit var btnAddGear: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main_screen)
+
+        // Typecasting
+        tvTotal = findViewById(R.id.tvTotal)
+        btnAddGear = findViewById(R.id.btnAddGear)
+
+
+        
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
