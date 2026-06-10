98754734f6a4d6f56df13b777f46b763b203fc34
TroyPeters19 <St10533034@myemeris.edu.za>
2026-06-10 10:46:24 +0200

    Declarations
    Variables are declared

--- IMAD_Exam/app/src/main/java/com/example/imad_exam/DetailedActivity.kt
+++ IMAD_Exam/app/src/main/java/com/example/imad_exam/DetailedActivity.kt
@@ -1,12 +1,24 @@
 package com.example.imad_exam
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class DetailedActivity : AppCompatActivity() {
+
+    // Declarations
+    private lateinit var spinner: Spinner
+    private lateinit var edtCategory: EditText
+    private lateinit var edtQuantity: EditText
+    private lateinit var edtComment: EditText
+    private lateinit var btnBack: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
