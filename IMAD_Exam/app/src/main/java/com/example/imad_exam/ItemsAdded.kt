60cc92b9bf2e44bc7c9ba6b2197414d7df959baa
TroyPeters19 <St10533034@myemeris.edu.za>
2026-06-10 11:08:02 +0200

    Data class updated
    Data classes for the item name, category, quantity and comment have been added. A "dataCaptured" class added to check if the user entered data for the item

--- IMAD_Exam/app/src/main/java/com/example/imad_exam/ItemsAdded.kt
+++ IMAD_Exam/app/src/main/java/com/example/imad_exam/ItemsAdded.kt
@@ -1,3 +1,19 @@
 package com.example.imad_exam
 
-data class ItemsAdded()
+data class ItemsAdded(
+
+    //Store the name of the item
+    val itemName : String,
+
+    //Stores the category(eg. Food,shelter or Safety)
+    var edtItemCategory: String,
+
+    //Stores the quantity of the item
+    var numItemQuantity: Int = 0,
+
+    //Stores the comment/note of the item
+    var edtItemComment: String = "",
+
+    //Tracks whether the user has entered data for the selected item
+    var dataCaptured: Boolean = false
+)
