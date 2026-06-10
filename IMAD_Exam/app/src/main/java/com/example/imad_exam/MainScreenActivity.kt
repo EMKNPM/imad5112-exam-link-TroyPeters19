0280d4b350dc811f45267aa0a89827b04413cc08
TroyPeters19 <St10533034@myemeris.edu.za>
2026-06-10 09:18:42 +0200

    MainScreenActivity and button
    The MainScreenActivity pages were created and the button was coded to take the user to the next screen

--- IMAD_Exam/app/src/main/java/com/example/imad_exam/MainScreenActivity.kt
+++ IMAD_Exam/app/src/main/java/com/example/imad_exam/MainScreenActivity.kt
@@ -1,37 +1,16 @@
 package com.example.imad_exam
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-
-    //Declarations
-    private lateinit var btnMain: Button
-    private lateinit var btnExit: Button
-
-
-
+class MainScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        //Typecasting
-        btnMain = findViewById(R.id.btnMain)
-        btnExit = findViewById(R.id.btnExit)
-
-
-        btnMain.setOnClickListener {
-            val intent = Intent(this, MainScreenActivity::class.java)
-            startActivity(intent)
-        }
-
-
+        setContentView(R.layout.activity_main_screen)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
